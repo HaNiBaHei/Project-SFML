c523cd48294e66932cc13b0df27346f96d1b79e5
CHINNAPAT SIRIYAJAI <63010231@KMITL.AC.TH>
2020-09-07 20:56:36 +0700

    update test walking

--- sfml test/test.cpp
+++ sfml test/test.cpp
@@ -1,22 +1,52 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(512, 512), "SFML works!", sf::Style::Close | sf::Style::Default);
+	sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
+	player.setFillColor(sf::Color::Red);
 
 	while (window.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		sf::Event evnt;
+		while (window.pollEvent(evnt))
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+			switch (evnt.type)
+			{
+			case sf::Event::Closed:
+			    window.close();
+					break;
+			case sf::Event::TextEntered:
+				if (evnt.text.unicode < 128)
+				{
+					printf("%c", evnt.text.unicode);
+				}
 
+			}
+			if (evnt.type == evnt.Closed)
+			{
+				window.close();
+			}
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+		{
+			player.move(-0.1f ,0.0f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+		{
+			player.move(0.1f ,0.0f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+		{
+			player.move(0.0f ,-0.1f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+		{
+			player.move(0.0f ,0.1f);
+		}
 		window.clear();
-		window.draw(shape);
+		window.draw(player);
 		window.display();
 	}
 
