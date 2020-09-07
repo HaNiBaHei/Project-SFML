2f41d42adc47b8584d7ed467d2c1b82a22a6da9e
CHINNAPAT SIRIYAJAI <63010231@KMITL.AC.TH>
2020-09-08 00:05:59 +0700

    update panquin

--- sfml test/test.cpp
+++ sfml test/test.cpp
@@ -4,9 +4,18 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(512, 512), "SFML works!", sf::Style::Close | sf::Style::Default);
-	sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
-	player.setFillColor(sf::Color::Red);
+	sf::RectangleShape player(sf::Vector2f(100.0f, 150.0f));
+	player.setPosition(206.0f, 206.0f);
+	sf::Texture playerTexture;
+	playerTexture.loadFromFile("test111.png");
+	player.setTexture(&playerTexture);
 
+	sf::Vector2u textureSize = playerTexture.getSize();
+	textureSize.x /= 3;
+	textureSize.y /= 9;
+
+	player.setTextureRect(sf::IntRect(textureSize.x * 2, textureSize.y * 2, textureSize.x, textureSize.y));
+	
 	while (window.isOpen())
 	{
 		sf::Event evnt;
