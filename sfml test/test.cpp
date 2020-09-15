0a40861647e8471a0d4b8080d6e14ac319989263
CHINNAPAT SIRIYAJAI <63010231@KMITL.AC.TH>
2020-09-16 02:07:53 +0700

    add animation

--- sfml test/test.cpp
+++ sfml test/test.cpp
@@ -1,23 +1,26 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "Animation.h"
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(512, 512), "SFML works!", sf::Style::Close | sf::Style::Default);
-	sf::RectangleShape player(sf::Vector2f(100.0f, 150.0f));
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!", sf::Style::Close | sf::Style::Default);
+	sf::RectangleShape player(sf::Vector2f(200.0f, 300.0f));
 	player.setPosition(206.0f, 206.0f);
 	sf::Texture playerTexture;
-	playerTexture.loadFromFile("test111.png");
+	playerTexture.loadFromFile("runleft.png");
 	player.setTexture(&playerTexture);
 
-	sf::Vector2u textureSize = playerTexture.getSize();
-	textureSize.x /= 3;
-	textureSize.y /= 9;
+	Animation animation(&playerTexture, sf::Vector2u(4, 3), 0.3f);
 
-	player.setTextureRect(sf::IntRect(textureSize.x * 2, textureSize.y * 2, textureSize.x, textureSize.y));
-	
+	float deltaTime = 0.0f;
+	sf::Clock clock;
+
 	while (window.isOpen())
 	{
+		
+		deltaTime = clock.restart().asSeconds();
+
 		sf::Event evnt;
 		while (window.pollEvent(evnt))
 		{
@@ -40,20 +43,24 @@ int main()
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
 		{
-			player.move(-0.1f ,0.0f);
+			player.move(-1.0f ,0.0f);
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
 		{
-			player.move(0.1f ,0.0f);
+			player.move(1.0f ,0.0f);
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
 		{
-			player.move(0.0f ,-0.1f);
+			player.move(0.0f ,-1.0f);
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
 		{
-			player.move(0.0f ,0.1f);
+			player.move(0.0f ,1.0f);
 		}
+		
+		animation.Update(1, deltaTime);
+		player.setTextureRect(animation.uvRect);
+		
 		window.clear();
 		window.draw(player);
 		window.display();
